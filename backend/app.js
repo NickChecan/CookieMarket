16af608bb1864e985cef89d8fe0075005ebc2cbd
Nicholas <nicholas.checan@gmail.com>
2018-11-28 19:34:12 -0200

    Initial Commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Database
+// db = container service
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
+
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+    //cookie: { type: String, required: false}
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
+// Start Server
+server.listen(3000)
